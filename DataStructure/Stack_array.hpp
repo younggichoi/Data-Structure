c75bed74294bb1ec0c112a729dc0fd6865117c01
kevin <younggichoi@snu.ac.kr>
2024-10-21 15:30:52 +0900

    finish stack array

--- DataStructure/Stack_array.hpp
+++ DataStructure/Stack_array.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 
 template <typename Type>
 class Stack
@@ -15,3 +16,51 @@ public:
 	void push(Type const&);
 	Type pop();
 };
+
+// constructor
+
+template <typename Type>
+Stack<Type>::Stack(int cap)
+	: stack_size(0), array_capacity(cap)
+{
+	array = new Type[cap];
+}
+
+// destructor
+
+template <typename Type>
+Stack<Type>::~Stack()
+{
+	delete[] array;
+}
+
+// member functions of Stack
+
+template <typename Type>
+bool Stack<Type>::empty() const
+{
+	return stack_size == 0;
+}
+
+template <typename Type>
+Type Stack<Type>::top() const
+{
+	assert(!empty());
+	return array[stack_size - 1];
+}
+
+template <typename Type>
+Type Stack<Type>::pop()
+{
+	assert(!empty());
+	stack_size--;
+	return array[stack_size];
+}
+
+template <typename Type>
+void Stack<Type>::push(Type const& obj)
+{
+	assert(stack_size != array_capacity);
+	array[stack_size] = obj;
+	++stack_size;
+}
