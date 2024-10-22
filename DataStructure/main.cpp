2c2551c1a951c8f6a5f00bb9fff515ac103ef595
kevin <younggichoi@snu.ac.kr>
2024-10-22 14:30:11 +0900

    queue list complete

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
-#include "Stack_array.hpp"
+#include "Queue-list.hpp"
 #include <cstdlib>
 #include <ctime>
 
 int main()
 {
 	using namespace std;
-	srand(time(0));
-	Stack<int> stack(1000);
-	int temp;
-	for (int i = 0; i < 1000; i++)
-	{
-		temp = rand();
-		stack.push(temp);
-		assert(temp == stack.top());
-	}
-	for (int i = 0; i < 1000; i++)
-	{
-		temp = stack.top();
-		assert(temp == stack.pop());
-	}
-	cout << "All test pass!" << endl;
+
 	return 0;
 }
