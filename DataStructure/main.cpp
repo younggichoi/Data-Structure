3f236d660de6abd71a7e800807ae046b04860d11
kevin <younggichoi@snu.ac.kr>
2024-10-22 17:33:29 +0900

    Parent Tree complete

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -6,15 +6,25 @@
 int main()
 {
 	using namespace std;
-	AbstractTree<char> tree('A');
-	AbstractTree<char>* b = tree.attach('B');
-	AbstractTree<char>* c = tree.attach('C');
-	AbstractTree<char>* d = tree.attach('D');
-	AbstractTree<char>* e = b->attach('E');
-	AbstractTree<char>* f = b->attach('F');
-	AbstractTree<char>* g = d->attach('G');
 
-	print_dfs_tree(&tree);
+	int const n = 20;
+	int parent_array[n] = { 0,0,0,0,0,2,2,2,3,3,4,4,4,4,5,5,10,12,12,15 };
+
+	AbstractTree<int>* root_node = nullptr;
+	AbstractTree<int>** array = new AbstractTree<int>*[n];
+
+	for (int i = 0; i < n; ++i)
+		array[i] = new AbstractTree<int>(i);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (parent_array[i] == i)
+			root_node = array[i];
+		else
+			array[parent_array[i]]->attach_subtree(array[i]);
+	}
+
+	print_dfs_tree(root_node);
 
 	return 0;
 }
